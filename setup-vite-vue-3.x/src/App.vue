9c880332d6a0d245b7b44fe69041e0d92530de08
chenlichao <chenlichao-khjy@bestpay.com.cn>
2021-01-25 15:18:37 +0800

    feat: setup-vite-vue-3.x

---
+++ setup-vite-vue-3.x/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<section>
+  <router-view></router-view>
+</section>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ setup-vite-vue-3.x/src/views/home.vue
@@ -0,0 +1,20 @@
+<template>
+<section>
+  <hello-world :msg="'charlies vue app'" />
+</section>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import HelloWorld from '/@/components/HelloWorld.vue'
+  export default defineComponent({
+    name: 'Home',
+    components: {
+      HelloWorld
+    }
+  });
+</script>
+
+<style>
+
+</style>
